c692b8fe3ba3efadadd770d074bd4d2fb026d9aa
ceas <edu.alves1327@gmail.com>
2025-02-09 23:35:52 -0300

    deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+//const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
